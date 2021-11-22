b8c0c7ab7a737c3426eb17877f6987298d5385f5
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-11-22 00:06:28 -0300

    Manipulando CSS Styles

--- script.js
+++ script.js
@@ -10,10 +10,19 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value)
   console.log(guess)
 
+  //Quando não há entrada
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ Número incorreto'
+
+    //Quando o jogador ganha
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Número correto'
+    //Manipulação de CSS
+    document.querySelector('body').style.backgroundColor = '#60b347'
+
+    document.querySelector('.number').style.width = '30rem'
+
+    //Quando o jogador perde
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Muito alto'
@@ -23,6 +32,8 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = '🙃 Você perdeu o jogo!'
       document.querySelector('.score').textContent = 0
     }
+
+    //Quando o jogador perde
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Muito baixo'
