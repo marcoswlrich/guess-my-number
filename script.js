dbc0544555d0a76c836815e2c78074b1f3a10803
Marcos Wlrich <marcosapw.dev@gmail.com>
2021-11-22 00:26:46 -0300

    Add funcionalidade de jogar novamente

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // document.querySelector('.message').textContent = '🎉 Número correto'
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1)
+let secretNumber = Math.trunc(Math.random() * 20 + 1)
 
 let score = 20
 
@@ -20,7 +20,6 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber
     //Manipulação de CSS
     document.querySelector('body').style.backgroundColor = '#60b347'
-
     document.querySelector('.number').style.width = '30rem'
 
     //Quando o jogador perde
@@ -45,4 +44,16 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0
     }
   }
+})
+
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20
+  secretNumber = Math.trunc(Math.random() * 20 + 1)
+
+  document.querySelector('.message').textContent = 'Comece a adivinhar...'
+  document.querySelector('.score').textContent = score
+  document.querySelector('.number').textContent = '?'
+  document.querySelector('.guess').value = ''
+  document.querySelector('body').style.backgroundColor = '#222'
+  document.querySelector('.number').style.width = '15rem'
 })
